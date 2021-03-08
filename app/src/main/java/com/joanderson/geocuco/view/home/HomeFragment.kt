01f4d54d7c427a755270d8581bb75292bbf3a0b4
Joanderson <joandersongsantos@yahoo.com>
2021-03-08 15:27:19 -0300

    feat: add navigation from HomeFragment to CreateAlarmFragment

--- app/src/main/java/com/joanderson/geocuco/view/home/HomeFragment.kt
+++ app/src/main/java/com/joanderson/geocuco/view/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.joanderson.geocuco.data.model.GeoAlarm
 import com.joanderson.geocuco.databinding.FragmentHomeBinding
 
@@ -25,5 +26,14 @@ class HomeFragment : Fragment() {
         binding.rvAlarmsOn.adapter = alarmAdapter
         binding.rvAlarmsOff.adapter = alarmAdapter
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.createNewGeoAlarm.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToCreateAlarmFragment()
+            findNavController().navigate(action)
+        }
     }
 }
